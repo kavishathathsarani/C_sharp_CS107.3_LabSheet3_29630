be8598c984efad06f972e6bbea04393cd23e8d1f
kavishathathsarani_USER_ <kaviherath377@gmail.com_USER_@DESKTOP-2SR3E2E>
2024-02-17 08:41:26 -0800

    upload

---
+++ Q2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee(101, "John Doe", 50000.00);
+
+            Console.WriteLine("Employee Information ");
+            employee.DisplayEmployeeInfo();
+
+        }
+    }
+}
+
+internal class Employee
+{
+    public int employeeID;
+    public string FullName;
+    public double salary;
+
+    public Employee(int employeeID, string FullName, double salary)
+    {
+        this.employeeID = employeeID;
+        this.FullName = FullName;
+        this.salary = salary;
+    }
+}
