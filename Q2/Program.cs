49f7f127d119efdf05eabc5083f5aa09684fafdf
kavishathathsarani_USER_ <kaviherath377@gmail.com_USER_@DESKTOP-2SR3E2E>
2024-02-18 06:17:16 -0800

    upload labsheet3 2

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Q2
 {
     internal class Program
-    {
+    { 
         static void Main(string[] args)
         {
             Employee employee = new Employee(101, "John Doe", 50000.00);
@@ -15,6 +15,15 @@ namespace Q2
             Console.WriteLine("Employee Information ");
             employee.DisplayEmployeeInfo();
 
+            Console.WriteLine($"Employee ID {employee.EmployeedID}");
+
+            employee.FullName = "Jane Smith";
+            Console.WriteLine($"Updated Full Name {employee.FullName}");
+
+            employee.Salary = 55000;
+            Console.WriteLine($"Updated salary  {employee.salary}");
+
+            Console.ReadLine();
         }
     }
 }
@@ -22,13 +31,33 @@ namespace Q2
 internal class Employee
 {
     public int employeeID;
-    public string FullName;
+    public string fullName;
     public double salary;
 
     public Employee(int employeeID, string FullName, double salary)
     {
         this.employeeID = employeeID;
-        this.FullName = FullName;
+        this.fullName = FullName;
         this.salary = salary;
+    }
+    public int EmployeedID => employeeID;
+
+    public string FullName
+    {
+        get => FullName;
+        set => FullName = value;
+
+    }
+    public double Salary
+    {
+        get => salary;
+        set => salary = value;
+    }
+
+    public void DisplayEmployeeInfo()
+    {
+        Console.WriteLine($"Employee ID {employeeID}");
+        Console.WriteLine($"Full Name {employeeID}");
+        Console.WriteLine($"Salary {employeeID}");
     }
 }
